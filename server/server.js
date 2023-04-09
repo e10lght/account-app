e601b7188e509f082e5c399aa4b7f1896c246f1c
ito <kira_neymar@icloud.com>
2023-04-09 22:55:50 +0900

    サーバ側も

---
+++ server/server.js
@@ -0,0 +1,32 @@
+require('dotenv').config()
+const PORT = process.env.PORT || 3008;
+const express = require('express')
+const app = express();
+const Router = require('./routes/route')
+const path = require('path')
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    if (req.headers.host) {
+        req.headers.host = 'account-app-client.herokuapp.com ';
+    }
+    next();
+});
+app.use(express.json());
+
+// Reactアプリケーションのビルドディレクトリを指定します。
+app.use(express.static(path.join(__dirname, '../client/build')));
+
+app.use('/api', Router)
+
+// すべてのリクエストをReactアプリケーションにルーティングします。
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+});
+
+
+app.listen(PORT, () => {
+    console.log('起動しました')
+})
